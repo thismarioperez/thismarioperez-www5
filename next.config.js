5732b1cecdd105b1e164a8d50f0f7d821f7f721c
Mario Perez <mario.perez415@gmail.com>
2022-08-16 00:30:40 -0400

    Add app styles

--- next.config.js
+++ next.config.js
@@ -1,17 +1,22 @@
-const createNextPluginPreval = require('next-plugin-preval/config');
+const path = require("path");
+const createNextPluginPreval = require("next-plugin-preval/config");
 const withNextPluginPreval = createNextPluginPreval();
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: '/home',
-        destination: '/',
-        permanent: true,
-      },
-    ]
-  },
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    async redirects() {
+        return [
+            {
+                source: "/home",
+                destination: "/",
+                permanent: true,
+            },
+        ];
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, "styles")],
+        prependData: `@import "styles/global.scss";`,
+    },
+};
 
-module.exports = withNextPluginPreval(nextConfig)
+module.exports = withNextPluginPreval(nextConfig);
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,11 @@
+// styles
+import "@/styles/app.scss";
+import "@fontsource/source-code-pro/200.css";
+import "@fontsource/source-code-pro/200-italic.css";
+import "@fontsource/source-code-pro/400.css";
+import "@fontsource/source-code-pro/400-italic.css";
+import "@fontsource/source-code-pro/700.css";
+import "@fontsource/source-code-pro/700-italic.css";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
