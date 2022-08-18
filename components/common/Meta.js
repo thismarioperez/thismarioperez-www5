e13dc1daee0baf68fe8718d784bb2ac851c91c4f
Mario Perez <mario.perez415@gmail.com>
2022-08-18 02:11:11 -0400

    Update site wide meta

--- components/common/Meta.js
+++ components/common/Meta.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { HOME_OG_IMAGE_URL } from "@/scripts/core/constants";
 
 export default function Meta({ children }) {
     return (
@@ -8,7 +7,6 @@ export default function Meta({ children }) {
             <link rel="icon" type="image/png" href="/favicon/favicon.png" />
             <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
             <meta name="description" content={`thismarioperez`} />
-            <meta property="og:image" content={HOME_OG_IMAGE_URL} />
             {children}
         </Head>
     );
