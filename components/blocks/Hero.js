ab68512394796e6f85a002ceaf27f0f392a00e3c
Mario Perez <mario.perez415@gmail.com>
2022-08-18 17:54:44 -0400

    Move Hero. Assemble content. Add PropTypes

---
+++ components/blocks/Hero.js
@@ -0,0 +1,51 @@
+import colors from "@/styles/exports/colors.module.scss";
+
+// lib
+import cx from "classnames";
+import PropTypes from "prop-types";
+
+// components
+import Buttons from "@/components/common/Buttons";
+import ContentWrapper from "@/components/common/ContentWrapper";
+import Section from "@/components/common/Section";
+
+const HeroPropTypes = {
+    buttons: PropTypes.arrayOf(PropTypes.shape({ value: PropTypes.string })),
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    content: PropTypes.string,
+    backgroundColor: PropTypes.shape({
+        value: PropTypes.oneOf(Object.keys(colors)),
+    }),
+};
+
+function Hero({ buttons, title, subtitle, content, backgroundColor, ...rest }) {
+    return (
+        <Section
+            className={cx(`-bg--${backgroundColor.value}`, "-tall")}
+            {...rest}
+        >
+            <div className="container">
+                {title && (
+                    <ContentWrapper>
+                        <h1>{title}</h1>
+                    </ContentWrapper>
+                )}
+                {subtitle && (
+                    <ContentWrapper>
+                        <h3>{subtitle}</h3>
+                    </ContentWrapper>
+                )}
+                {buttons && buttons.length > 0 && (
+                    <ContentWrapper>
+                        <Buttons buttons={buttons} />
+                    </ContentWrapper>
+                )}
+            </div>
+        </Section>
+    );
+}
+
+Hero.propTypes = HeroPropTypes;
+
+export default Hero;
