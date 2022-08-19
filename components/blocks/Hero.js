7c190140387a82ccc1d570f74f06ad09a6363394
Mario Perez <mario.perez415@gmail.com>
2022-08-19 00:08:00 -0400

    Fix import

--- components/blocks/Hero.js
+++ components/blocks/Hero.js
@@ -8,7 +8,7 @@ import PropTypes from "prop-types";
 import Buttons from "@/components/common/Buttons";
 import ContentWrapper from "@/components/common/ContentWrapper";
 import Section from "@/components/common/Section";
-import MarkdownRenderer from "../common/MarkdownRenderer";
+import MarkdownRenderer from "@/components/common/MarkdownRenderer";
 
 const HeroPropTypes = {
     buttons: PropTypes.arrayOf(PropTypes.shape({ value: PropTypes.string })),
@@ -40,7 +40,7 @@ function Hero({ buttons, title, subtitle, content, backgroundColor, ...rest }) {
                 {content && (
                     <ContentWrapper>
                         <div className="-exp">
-                        <MarkdownRenderer>{content}</MarkdownRenderer>
+                            <MarkdownRenderer>{content}</MarkdownRenderer>
                         </div>
                     </ContentWrapper>
                 )}
