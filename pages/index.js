4b4671da81246240351eb2ea2fe30c479e1b3c3f
Mario Perez <mario.perez415@gmail.com>
2022-08-19 13:10:13 -0400

    Move blocks

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { constants } from "@/scripts/core";
 
 // components
 import Meta from "@/components/common/Meta";
-import Blocks from "@/components/blocks";
+import Blocks from "@/components/_blocks";
 
 export default function Home({ data }) {
     log("log", "Home Page Data:", data);
