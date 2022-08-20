5a6d91960ee8f8b51297356cfd56d488b782ecc2
Mario Perez <mario.perez415@gmail.com>
2022-08-20 01:52:57 -0400

    Allow camelCase css names

--- next.config.js
+++ next.config.js
@@ -17,6 +17,20 @@ const nextConfig = {
         includePaths: [path.join(__dirname, "styles")],
         prependData: `@import "styles/global.scss";`,
     },
+	webpack: (config) => {
+		// camel-case style names from css modules
+		config.module.rules
+			.find(({oneOf}) => !!oneOf).oneOf
+			.filter(({use}) => JSON.stringify(use)?.includes('css-loader'))
+			.reduce((acc, {use}) => acc.concat(use), [])
+			.forEach(({options}) => {
+				if (options.modules) {
+					options.modules.exportLocalsConvention = 'camelCase';
+				}
+			});
+
+		return config;
+	},
 };
 
 module.exports = withNextPluginPreval(nextConfig);
