fb28a7ad898b4126d1619e6cde2a8fab09760e8e
Mario Perez <mario.perez415@gmail.com>
2022-08-21 15:54:33 -0400

    Add frontend/backend github urls

--- scripts/core/constants.js
+++ scripts/core/constants.js
@@ -1,6 +1,8 @@
 export const SITE_NAME = "thismarioperez";
-export const GITHUB_URL =
+export const FRONTEND_GITHUB_URL =
     "https://github.com/thismarioperez/thismarioperez-www5";
+export const BACKEND_GITHUB_URL =
+    "https://github.com/thismarioperez/thismarioperez-cms";
 export const API_URL = process.env.API_URL;
 export const API_TOKEN = process.env.API_TOKEN;
 export const BLOCK_TYPE = {
