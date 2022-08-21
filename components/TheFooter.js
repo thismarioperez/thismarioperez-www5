1647e3abc640aa9da62d889800b8705f32e53386
Mario Perez <mario.perez415@gmail.com>
2022-08-21 15:54:48 -0400

    Only show repos and social links in footer

--- components/TheFooter.js
+++ components/TheFooter.js
@@ -1,37 +1,39 @@
 // lib
 import { constants } from "@/scripts/core";
 
-const { GITHUB_URL } = constants;
+const { FRONTEND_GITHUB_URL, BACKEND_GITHUB_URL } = constants;
 
 // components
-import Contact from "@/components/common/Contact";
+import Social from "@/components/common/Social";
 import Container from "@/components/common/Container";
 import ContentWrapper from "@/components/common/ContentWrapper";
 import Section from "./common/Section";
 
 export default function Footer() {
     return (
-        <footer className="-bg--secondary">
-            <Section>
-                <Container>
-                    <ContentWrapper className="-exp--b">
-                        <h2>Get in touch:</h2>
-                        <Contact />
-                    </ContentWrapper>
-                    <ContentWrapper>
-                        <h3>Built with Next.JS and Strapi.</h3>
-                        <p>
-                            <a
-                                href={GITHUB_URL}
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                View on GitHub
-                            </a>
-                        </p>
-                    </ContentWrapper>
-                </Container>
-            </Section>
+        <footer className="-bg--secondary -exp">
+            <Container>
+                <Social/>
+                <h3>
+                    Built with{" "}
+                    <a
+                        href={FRONTEND_GITHUB_URL}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Next.JS
+                    </a>{" "}
+                    and{" "}
+                    <a
+                        href={BACKEND_GITHUB_URL}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Strapi
+                    </a>
+                    .
+                </h3>
+            </Container>
         </footer>
     );
 }
