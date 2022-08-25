1b58d54cc3771cbf7da953c16c01efc452a0a250
Mario Perez <mario.perez415@gmail.com>
2022-08-25 00:05:55 -0400

    Render new home page content

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,80 @@
 // lib
-import { getPageBySlug } from "@/lib/api";
+import { getHomePage } from "@/lib/api";
 import { log } from "@/scripts/core";
 import { constants } from "@/scripts/core";
+import cx from "classnames";
 
 // components
 import Meta from "@/components/common/Meta";
 import TheHomeScene from "@/components/TheHomeScene";
+import Section from "@/components/common/Section";
+import ContentWrapper from "@/components/common/ContentWrapper";
+import Buttons from "@/components/common/Buttons";
+import Container from "@/components/common/Container";
+import Copy from "@/components/common/Copy";
+import MarkdownRenderer from "@/components/common/MarkdownRenderer";
 
 export default function Home({ data }) {
     log("log", "Home Page Data:", data);
     const {
-        attributes: { title },
+        attributes: { title, header, content, subheader, buttons },
     } = data;
     return (
         <>
             <Meta>
                 <title>{`${constants.SITE_NAME} | ${title}`}</title>
             </Meta>
-            <TheHomeScene text="M"/>
+            <Section className="-tall">
+                <Container>
+                    <Copy className="-z--2">
+                        {header?.length > 0 && (
+                            <ContentWrapper
+                                className={cx(
+                                    (subheader?.length ||
+                                        content?.length ||
+                                        buttons?.length) &&
+                                        "-exp--b--1/2"
+                                )}
+                            >
+                                <h1>{header}</h1>
+                            </ContentWrapper>
+                        )}
+                        {subheader?.length > 0 && (
+                            <ContentWrapper
+                                className={cx(
+                                    (content?.length || buttons?.length) &&
+                                        "-exp--b"
+                                )}
+                            >
+                                <h3>{subheader}</h3>
+                            </ContentWrapper>
+                        )}
+                        {content?.length > 0 && (
+                            <ContentWrapper
+                                className={cx(buttons?.length && "-exp--b")}
+                            >
+                                <MarkdownRenderer>{content}</MarkdownRenderer>
+                            </ContentWrapper>
+                        )}
+                        {buttons?.length > 0 && (
+                            <ContentWrapper>
+                                <Buttons buttons={buttons} />
+                            </ContentWrapper>
+                        )}
+                    </Copy>
+                </Container>
+            </Section>
+            <TheHomeScene text="M" />
         </>
     );
 }
 
 export async function getServerSideProps() {
-    const { pages } = await getPageBySlug("home");
+    const { homePage } = await getHomePage();
 
     return {
         props: {
-            data: pages?.data[0] ?? {},
+            data: homePage?.data ?? {},
         },
     };
 }
