02c2cda5ed4d69b43bc585fc6449646231065d1b
Mario Perez <mario.perez415@gmail.com>
2022-08-25 03:20:43 -0400

    Setup float

--- components/TheHomeScene/TheHomeScene.js
+++ components/TheHomeScene/TheHomeScene.js
@@ -1,7 +1,7 @@
 import styles from "./TheHomeScene.module.scss";
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stats } from "@react-three/drei";
+import { Float, OrbitControls, Stats } from "@react-three/drei";
 import { Suspense } from "react";
 
 import Lighting from "./Lighting";
@@ -9,7 +9,7 @@ import Camera from "./Camera";
 import TextMesh from "./TextMesh";
 
 export default function TheHomeScene() {
-    const debug = true;
+    const debug = false;
 
     return (
         <div className={styles.wrapper}>
@@ -19,17 +19,21 @@ export default function TheHomeScene() {
                     {debug ? <axesHelper args={[2]} /> : null}
                     {debug ? <gridHelper args={[20, 20]} /> : null}
                     {debug ? <OrbitControls /> : null}
-                    <Camera debug={debug} position={[2, 2, 4]} makeDefault/>
+                    {/* <Camera debug={debug} position={[2, 2, 4]} makeDefault/> */}
+                    <Camera debug={debug} position={[0, 0, 4]} makeDefault/>
                     <Lighting debug={debug} />
                     <Suspense fallback={null}>
-                        <TextMesh
-                            font="/fonts/Prestige Elite Std_Bold.json"
-                            scale={2}
-                            debug={debug}
-                            position={[1, -0.5, 0]}
-                        >
-                            {"M"}
-                        </TextMesh>
+                        <Float>
+                            <TextMesh
+                                font="/fonts/Prestige Elite Std_Bold.json"
+                                scale={2}
+                                debug={debug}
+                                position={[1, -0.5, 0]}
+                            >
+                                {"M"}
+                            </TextMesh>
+
+                        </Float>
                     </Suspense>
                 </Canvas>
             </div>
