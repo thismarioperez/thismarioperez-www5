af36ff31a975df35f4063395fbf9ccba34a874f2
Mario Perez <mario.perez415@gmail.com>
2022-08-24 22:41:25 -0400

    Setup threejs transpiling

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const withTM = require("next-transpile-modules")(["three"]);
 const createNextPluginPreval = require("next-plugin-preval/config");
 const withNextPluginPreval = createNextPluginPreval();
 const nextConfig = {
@@ -17,23 +18,25 @@ const nextConfig = {
         includePaths: [path.join(__dirname, "styles")],
         prependData: `@import "styles/global.scss";`,
     },
-	webpack: (config) => {
-		// camel-case style names from css modules
-		config.module.rules
-			.find(({oneOf}) => !!oneOf).oneOf
-			.filter(({use}) => JSON.stringify(use)?.includes('css-loader'))
-			.reduce((acc, {use}) => acc.concat(use), [])
-			.forEach(({options}) => {
-				if (options.modules) {
-					options.modules.exportLocalsConvention = 'camelCase';
-				}
-			});
+    webpack: (config) => {
+        // camel-case style names from css modules
+        config.module.rules
+            .find(({ oneOf }) => !!oneOf)
+            .oneOf.filter(({ use }) =>
+                JSON.stringify(use)?.includes("css-loader")
+            )
+            .reduce((acc, { use }) => acc.concat(use), [])
+            .forEach(({ options }) => {
+                if (options.modules) {
+                    options.modules.exportLocalsConvention = "camelCase";
+                }
+            });
 
-		return config;
-	},
+        return config;
+    },
     images: {
         domains: [`${process.env.AWS_BUCKET}.s3.amazonaws.com`],
-    }
+    },
 };
 
-module.exports = withNextPluginPreval(nextConfig);
+module.exports = withNextPluginPreval(withTM(nextConfig));
