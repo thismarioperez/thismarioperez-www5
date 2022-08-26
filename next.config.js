5fb073f500cf998f4ca8b1c6993fd7f80431d4cd
Mario Perez <mario.perez415@gmail.com>
2022-08-25 23:45:29 -0400

    Remove unnecessary redirect

--- next.config.js
+++ next.config.js
@@ -5,15 +5,6 @@ const withNextPluginPreval = createNextPluginPreval();
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    async redirects() {
-        return [
-            {
-                source: "/home",
-                destination: "/",
-                permanent: true,
-            },
-        ];
-    },
     sassOptions: {
         includePaths: [path.join(__dirname, "styles")],
         prependData: `@import "styles/global.scss";`,
