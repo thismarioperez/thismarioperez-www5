21fb4f8379db5af8fb029cd10a1a163b1c65b737
Mario Perez <mario.perez415@gmail.com>
2022-08-26 13:37:43 -0400

    Remove social from footer

--- components/TheFooter.js
+++ components/TheFooter.js
@@ -4,16 +4,12 @@ import { constants } from "@/scripts/core";
 const { FRONTEND_GITHUB_URL, BACKEND_GITHUB_URL } = constants;
 
 // components
-import Social from "@/components/common/Social";
 import Container from "@/components/common/Container";
-import ContentWrapper from "@/components/common/ContentWrapper";
-import Section from "./common/Section";
 
 export default function Footer() {
     return (
         <footer className="-bg--secondary -exp">
             <Container>
-                <Social/>
                 <h3>
                     Built with{" "}
                     <a
