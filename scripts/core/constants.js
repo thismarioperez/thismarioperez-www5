123d0ff03bc4d22bdae9a673c112202d0ee2068c
Mario Perez <mario.perez415@gmail.com>
2022-08-30 20:28:43 -0400

    Update env var references

--- scripts/core/constants.js
+++ scripts/core/constants.js
@@ -3,8 +3,9 @@ export const FRONTEND_GITHUB_URL =
     "https://github.com/thismarioperez/thismarioperez-www5";
 export const BACKEND_GITHUB_URL =
     "https://github.com/thismarioperez/thismarioperez-cms";
-export const API_URL = process.env.API_URL;
-export const API_TOKEN = process.env.API_TOKEN;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_TOKEN = process.env.NEXT_PUBLIC_API_TOKEN;
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 export const BLOCK_TYPE = {
     HERO: "ComponentBlocksHero",
     INTERIOR: "ComponentBlocksInterior",
